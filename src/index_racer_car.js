28ba9e50739b7b1255850fe5bb9a3a2fa11c1adb
Marizabelle Aldana <94816917+CatVirus22@users.noreply.github.com>
2022-10-12 21:19:22 -0600

    changes made to Js and CSS

--- src/index_racer_car.js
+++ src/index_racer_car.js
@@ -47,7 +47,7 @@ function startBoard() {
   for (let x = 0; x < 14; x++) {
     let div = document.createElement("div");
     div.setAttribute("class", "road");
-    div.style.top = x * 100 + "px";
+    div.style.top = x * 50 + "px";
     div.style.width = player.roadwidth + "px";
     container.appendChild(div);
   }
@@ -78,7 +78,7 @@ function moveRoad() {
   let prevRoad = tempRoad[0].offsetLeft;
   for (let x = 0; x < tempRoad.lenght; x++) {
     let num = tempRoad[x].offsetTop + player.speed;
-    if (num > 600) {
+    if (num > 700) {
     }
     tempRoad[x].style.top = num + "px";
   }
@@ -87,21 +87,24 @@ function moveRoad() {
 function playGame() {
   if (gamePlay) {
     updateDash();
-    moveRoad();
+
     ///movementcar
+
+    moveRoad();
+
     if (keys.ArrowUp) {
       if (player.ele.y > 100) player.ele.y -= 1;
       player.speed = player.speed < 20 ? player.speed + 0.07 : 20;
     }
     if (keys.ArrowDown) {
-      if (player.ele.y < 600) player.ele.y += 1;
+      if (player.ele.y < 500) player.ele.y += 1;
       player.speed = player.speed > 0 ? player.speed - 0.2 : 0;
     }
     if (keys.ArrowRight) {
-      player.ele.x += player.speed;
+      player.ele.x += player.speed / 2;
     }
     if (keys.ArrowLeft) {
-      player.ele.x -= player.speed;
+      player.ele.x -= player.speed / 2;
     }
     ///movecar
 
